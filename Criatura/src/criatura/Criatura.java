46f000cbd85205bb648d2ed9604d59a256fddf8a
JAIME <JAIME@DESKTOP-N4Q0PTU>
2025-04-08 20:06:49 -0500

    Clase Abstracta Criatura:

--- Criatura/src/criatura/Criatura.java
+++ Criatura/src/criatura/Criatura.java
@@ -2,14 +2,29 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package criatura;
+public abstract class Criatura {
+    String nombre;
+    protected int salud;
+    protected int fuerza;
 
-
-public class Criatura {
-
-    
-    public static void main(String[] args) {
-        
+    public Criatura(String nombre, int salud, int fuerza) {
+        this.nombre = nombre;
+        this.salud = salud;
+        this.fuerza = fuerza;
     }
-    
+
+    public boolean estaViva() {
+        return salud > 0;
+    }
+
+    public abstract void atacar(Criatura objetivo);
+    public abstract void defender(int dano);
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getSalud() {
+        return salud;
+    }
 }
